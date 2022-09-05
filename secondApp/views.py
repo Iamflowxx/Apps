7570dcf28b2ea0de4cc40e0ec462854774034482
Iamflowxx <kevincolombino23@gmail.com>
2022-09-05 16:42:15 -0300

    segunda App terminada

---
+++ secondApp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def saludo(request):
+    return HttpResponse("<h1> Ejemplo app2 2 vista 1 </h1>")
